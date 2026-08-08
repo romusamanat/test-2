092e55085bea79643d452df3726a40ce296e8c13
romusamanat <romusamanatov753@gmail.com>
2026-08-08 15:47:00 +0600

    first commit

---
+++ man.js
@@ -0,0 +1,15 @@
+// let name = ("jhon");
+// let age = ("20");
+// console.log ()
+
+// Text.toUpperCase()
+// (prompt("Введите свое имя!")) 
+// alert("ramis")
+
+const student = {
+    name: "ramis",
+    age: "19",
+    hobby: "Football"
+};
+ student.language = "javaScript";
+ console.log()
